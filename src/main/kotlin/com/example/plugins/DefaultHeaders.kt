0a7cab916af60441e8d713a4d07442f4fcda4f03
priyanshuborole <ppborole1762002@gmail.com>
2023-06-09 13:43:48 +0530

    installed required plugins

---
+++ src/main/kotlin/com/example/plugins/DefaultHeaders.kt
@@ -0,0 +1,15 @@
+package com.example.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.defaultheaders.*
+import java.time.Duration
+
+fun Application.configureDefaultHeaders(){
+    install(DefaultHeaders)
+    val oneYearInSeconds = Duration.ofDays(365).seconds
+    headersOf(
+        name = HttpHeaders.CacheControl,
+        value = "public, max-age=$oneYearInSeconds, immutable"
+    )
+}
